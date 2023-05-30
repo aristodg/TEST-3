7a0b5f5f75e8c74b2d120b3a0fac01ecac7f6310
Aristoteles Dominguez <adominguez@MackayComm.com>
2023-05-30 18:15:50 +0200

    Wait -- 6

--- src/js/main.js
+++ src/js/main.js
@@ -8,3 +8,14 @@ function pruebaES5(){
     sleepES5(3000);    //Dormimos la ejecución durante 3 segundos
     console.log('Fin de la función de prueba.');
 };
+
+var sleep = function(ms){
+    //Crea una promesa y la resuleve automáticamente con un timeout a los ms segundos
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+async function pruebaES6(){
+    console.log('Inicio de la función de prueba.');
+    await sleep(3000);    //Dormimos la ejecución durante 3 segundos
+    console.log('Fin de la función de prueba.');
+};
