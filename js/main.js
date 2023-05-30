87e09204030bd70813222d8ea71cb74291b6a6c0
Aristoteles Dominguez <adominguez@MackayComm.com>
2023-05-30 17:43:23 +0200

    Testing javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-var testingThing="ddd";
+var sleepES5 = function(ms){
+    var esperarHasta = new Date().getTime() + ms;
+    while(new Date().getTime() < esperarHasta) continue;
+};
+
+function pruebaES5(){
+    console.log('Inicio de la función de prueba.');
+    sleepES5(3000);    //Dormimos la ejecución durante 3 segundos
+    console.log('Fin de la función de prueba.');
+};
